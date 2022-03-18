ec8784f343c00197267172d88e279fa03c59a7df
EmileGaudinot <emile.gaudinot@eleves.ec-nantes.fr>
2022-03-18 22:50:59 +0100

    Enlève les tabulation des .csv et remplace par des virgules

---
+++ csv_sans_tabulations.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 18 22:33:54 2022
+
+@author: emile
+
+Transforme un fichier .csv de nuage de points avec tabulations en fichier .csv
+classique avec des virgules
+"""
+
+
+
+import csv
+
+
+def conversion(nomSansExtension):
+    fileCsv = open(nomSansExtension + '.csv')
+    csvreader = csv.reader(fileCsv)    
+    fileTxt = open(nomSansExtension + 'SansTabulation.txt', 'w') 
+    
+    for row in csvreader:
+        if row != []: # dernière ligne est vide
+            ligne = row[0]
+            nouvelleLigne = ''
+            i = 0
+            for j in range(3):
+                mot = ''
+                while ligne[i] != '\t':
+                    mot += ligne[i]
+                    i += 1
+                
+                nouvelleLigne += mot
+                if j!= 2:
+                    nouvelleLigne += ','
+                i += 1
+            #on a récup les coordonnées
+            fileTxt.write(nouvelleLigne)
+            fileTxt.write('\n')
+                                      
+    fileCsv.close()
+    fileTxt.close()
+    
+    
+nom = 'resultline1'
+conversion(nom)
